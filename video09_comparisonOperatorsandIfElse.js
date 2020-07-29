863fd3d6984c3c5668ae0605978f38c6db18bfe3
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:16:23 -0300

    version1.0

---
+++ video09_comparisonOperatorsandIfElse.js
@@ -0,0 +1,36 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+//Comparison Operators & If... Else
+
+var isFCCGreate = true ;
+
+if ( isFCCGreate ) {
+
+    console . log ( " Free Code Camp is amazing! " ) ;
+
+} else {
+
+    console . log ( " Free Code Camp is horrible " ) ;
+
+}
+
+// Comparison Operators: < > <+ >= == !=
+
+var age = 18 ;
+
+if ( age >= 18 ) {
+
+    console . log ( " You are an adult " ) ;
+
+} else if ( age < 18 ) {
+
+    console . log ( " You are a child " ) ;
+
+} else if ( age < 2 ) {
+
+    console . log ( " You are a baby !" ) ;
+
+} ;
+
+if ( age == 18 ) { console . log ( " You are eighteen " ) } ;
+if ( age != 18 ) { console . log ( " You are not eighteen " ) } ;
+
