1088c061e72ea29d2bc48c8bbaa5893a17b45885
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:11:27 -0300

    version1.0

---
+++ video03_numbers.js
@@ -0,0 +1,90 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+// Numbers !
+
+var number = 12  ;
+
+console . log ( number ) ;
+
+// sum
+
+var number = 12 + 2 ;
+
+console . log ( number ) ;
+
+// subtract
+
+var number = 12 - 2 ;
+
+console . log ( number ) ;
+
+//multiplication
+
+var number = 12 * 2 ;
+console . log ( number ) ;
+
+// Division
+
+var number = 12 / 2 ;
+console . log ( number ) ;
+
+// Division with decimals
+
+var number = 12 / 7 ;
+console . log ( number ) ;
+
+//modulus operator
+
+var number = 12 % 7 ;
+console . log ( number ) ;
+
+// all operations with decimals
+
+var number = 12 + 7.3 ;
+console . log ( number ) ;
+
+// and
+
+var number = 1.2 / 7.3 ;
+console . log ( number ) ;
+
+// and 
+
+var number = 12 ;
+number ++ ;
+console . log ( number ) ;
+
+// and 
+
+var number = 12 ;
+number -- 
+console . log ( number ) ;
+
+// and
+
+var number = 12 ;
+number = number + 5 ;
+console . log ( number )  ;
+
+// and
+
+var number = 12 ;
+number += 5 ;
+console . log ( number ) ;
+
+// and 
+
+var number = 12 ;
+number /= 5 ;
+console . log ( number ) ;
+
+//and
+
+var number = 12 ;
+console . log ( number++ ) ;
+console . log ( number ) ;
+
+// and 
+
+var number = 12 ;
+console . log ( ++number ) ;
+
