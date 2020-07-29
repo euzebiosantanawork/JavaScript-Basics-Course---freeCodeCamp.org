4aeae0eee689d90c8b3607df0919abd13268e566
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:11:48 -0300

    version1.0

---
+++ video04_string.js
@@ -0,0 +1,25 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+// Strings
+
+var myName = ` Beau ` ;
+var sentence = " He said\"Hi!\" " ; // He said Hi!
+var sentence = ` He said "Hi!" ` ; // He said Hi!
+
+/*
+Code Output
+
+\' sigle quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+
+var fullName = " Euzebio " + " Santana " ; // Euzebio Santana
+
+var setence2 = " My name is " + fullName ; // My name is Euzebio Santana
+
+fullName += " is my name " ; // Euzebio Santana is my name ; 
