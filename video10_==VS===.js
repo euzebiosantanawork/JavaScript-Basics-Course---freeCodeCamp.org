617153d627d0d36d504105da18f3b1813a6630de
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:17:59 -0300

    version1.0

---
+++ video10_==VS===.js
@@ -0,0 +1,16 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+// JS Nuggets : == vs ===
+
+// == abstract equality
+
+//=== strict equality
+
+console . log ( 3 == " 3 " ) ;
+console . log ( 3 === " 3 " ) ;
+
+console . log ( true == ` 1 ` ) ;
+console . log ( true === ` 1 ` ) ;
+
+
+console . log ( ` This is a string ` == new String ( ` This is a string . ` ) ) ;
+console . log ( ` This is a string ` === new String ( ` This is a string . ` ) ) ;
