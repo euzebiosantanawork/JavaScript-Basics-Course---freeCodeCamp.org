4a34465771b72573b3aaa6547cf739c6b56df2a7
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:24:54 -0300

    version1.0

---
+++ video16_commonArrayMethods.js
@@ -0,0 +1,39 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+
+var arr = [ ` a ` , ` b ` , ` c ` ] ;
+
+arr . push ( ` d ` ) ;
+console . log ( arr ) ;
+
+console . log ( arr. pop() ) ;
+console . log ( arr.pop() ) ;
+console . log ( arr ) ;
+
+var arr2 = [ ` g ` , `  q ` ] ;
+console . log ( arr . concat ( arr2 ) ) ;
+
+console . log ( arr2 ) ;
+
+console . log ( arr . join ( `` ) ) ;
+
+console . log ( arr . reverse () ) ;
+console . log ( arr ) ;
+
+console . log ( arr . shift () ) ;
+
+console . log ( arr ) ;
+
+console . log ( arr . unshift ( ' p ' ) ) ;
+
+console . log ( arr );
+
+console . log ( arr.slice ( 1 , 2 ) ) ;
+
+arr . push ( ` i ` ) ;
+arr . push ( ` f ` ) ;
+arr . sort ( arr ) ;
+console . log ( arr ) ;
+
+console . log ( arr . splice ( 2 , 2 " JS Nuggets ")  ) ;
+console . log ( arr ) ;
+
