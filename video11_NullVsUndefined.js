bd04d00d5e440452a770ad1a3cc8d0977c07f8f3
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:19:42 -0300

    version1.0

---
+++ video11_NullVsUndefined.js
@@ -0,0 +1,22 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+// JS Nuggets : Null vs Undefined
+
+var test ;
+console . log ( test ) ;
+
+test = null ;
+console . log ( test ) ;
+
+console . log ( typeof null ) ;
+console . log ( typeof undefinded ) ;
+
+console . log ( null === undefined ) ;
+console . log ( null == undefined ) ;
+console . log ( null === null ) ;
+console . log ( null == null) ;
+console . log ( !null ) ;
+console . log ( null ) ;
+console . log ( !! null ) ;
+console . log ( 1 + null ) ;
+console . log ( 1 + undefined );
+
