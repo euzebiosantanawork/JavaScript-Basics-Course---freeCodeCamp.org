9e938d13808d1ddeba4b8917c2a45a063e2e0ab8
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:07:30 -0300

    version1.0

---
+++ video01_variables.js
@@ -0,0 +1,22 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+
+// Variables
+
+var myName ;
+/*
+var 
+const 
+let
+*/
+
+myName = "Beau" ;
+
+var state = "Michigan" ;
+
+console . log ( ' My name is: ' + myName ) ;
+
+state = " California " ;
+
+// 0-9 , a- z , A-Z
+
+var bestWebsiteEver = " freeCodeCamp " ;
