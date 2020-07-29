3020c8449d18dec850fca4c5a987d10e2e17bf0f
Euzebio <euzebiosantanawork@gmail.com>
2020-07-29 18:12:22 -0300

    version1.0

---
+++ video05_strings.js
@@ -0,0 +1,23 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+var firstName = " Beau " ;
+
+console . log ( firsName [ 0 ] ) ;
+
+var firstName = " Beau " ;
+
+console . log ( firsName [ 1 ] ) ;
+
+var firstName = " Beau " ;
+
+console . log ( firsName [ firstName . length - 1 ] ) ;
+
+var firstName = " Beau " ;
+
+console . log ( firsName [ firstName . length - 2 ] ) ;
+
+firstName [ 0 ] = "Q" ;
+
+firstName = " Quincy " ;
+
+console . log ( firstName ) ;
+ 
