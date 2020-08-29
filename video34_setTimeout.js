40aa228f4a42d20ab90781883987032d08c8aa0c
Euzebio <euzebiosantanawork@gmail.com>
2020-08-29 14:39:31 -0300

    version1.0

---
+++ video34_setTimeout.js
@@ -0,0 +1,28 @@
+// These classes and others can be found at: https://www.youtube.com/watch?v=le-URjBhevE&list=PLWKjhJtqVAbk2qRZtWSzCIN38JC_NdhW5&index=2&t=0s
+// setTimeout and setInterval
+
+
+// setTimeout
+
+setTimeout ( nye , 3000 ) ;
+
+cosole . log ( ` hello ` ) ;
+
+clearTimeout ( timeoutID ) ;
+
+function bye () {
+
+    console . log ( ` goodbye ` )
+
+}
+
+//setInterval
+
+var count = 0
+var intId = setIntervsl ( counter , 1000 ) ;
+
+function counter () {
+
+    console . log ( ++count ) ;
+
+}
